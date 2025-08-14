0412d236a9531f8920944adddf27898d4049e7f5
Rodrigo Baumann <rodrigo.baumann@jassy.ag>
2025-08-14 15:59:23 -0300

    add more middlewares

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,25 @@ const express = require ('express');
 
 const app = express();
 
-app.use((req, res) => {
-    res.send('Hello World');
+app.use((req, res, next) => {
+    console.log('Requête reçue!');
+    next();
+    
 });
+
+app.use((req, res, next) => {
+    res.status(201);
+    next();
+});
+
+app.use((req, res, next) => {
+    res.send('Hello World');
+    next(); 
+});
+
+app.use((req, res) => {
+    console.log('Requête termинé!');
+});
+
 
 module.exports = app;
